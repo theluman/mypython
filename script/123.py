1b4b3009f2a807e8b4c7dac87f7a606d72577341
theluman <435904632@qq.com>
2016-03-01 16:51:04 +0800

    sdsad

--- script/123.py
+++ script/123.py
@@ -18,10 +18,15 @@ if len(ips) <= 9:
     #输出9位字符，右对齐，补空格
     ips = ips.rjust(9) 
     #转换成大写
-    ops = chd[ips[0]]+und['Y']+chd[ips[1]]+und['Q']+chd[ips[2]]+und['B']+chd[ips[3]]+und['S']+chd[ips[4]]+und['W']+chd[ips[5]]+und['Q']+chd[ips[6]]+und['B']+chd[ips[7]]+und['S']+chd[ips[8]]+'元整'
+    ops = chd[ips[0]]+und['Y']+chd[ips[1]]+und['Q']+chd[ips[2]]+und['B']+chd[ips[3]]+und['S']+chd[ips[4]]+und['W']+chd[ips[5]]+und['Q']+chd[ips[6]]+und['B']+chd[ips[7]]+und['S']+chd[ips[8]]+'元'
     #无用的数字位替换为空
-    ops = re.sub("(m...)+","",ops)
-    if 
+    ops = re.sub('(m...)+','',ops)
+    #处理零
+    ops = re.sub('零元$','元',ops)
+    ops = re.sub('零万','万',ops)
+    ops = re.sub('(零...)+','零',ops)
+    ops = re.sub('零万','万',ops)
+    ops = re.sub('零元$','元',ops)
     print ops
 else:
     print 'The number is too big.'
