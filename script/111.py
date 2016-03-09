60e0573acc2e783a944fd0a9982a2ef8f152cf5f
theluman <435904632@qq.com>
2016-03-09 16:49:27 +0800

    sadas

--- script/111.py
+++ script/111.py
@@ -22,8 +22,9 @@ class ZHANGJIE:
     def getNum(self):
         webcon = self.getPage()
         Num = re.findall('<a href="/p/(\d+)"',webcon)
-        print Num
-        return Num
+        global Num_r
+        Num_r = Num[::-1]
+        return Num_r
 
 #每题内容类
 class NEIRONG:
@@ -43,25 +44,35 @@ class NEIRONG:
     def getTitle(self):
         webcon = self.getPage()
         title = re.search('style="width: 450px">(.*)</h1>',webcon).group(1)
-        #print title
         return title
 
     #获得小说内容并打印换行
     def getCon(self):
         webcon = self.getPage()
-        con = re.search('支持兰大，一定要记得投票哦！推荐票！(.*)(</div><br>)',webcon).group(1)
-        con_n = re.sub('<br>','\n',con)
-        return con_n
+        try:
+            con = re.search('支持兰大，一定要记得投票哦！推荐票！(.*)(</div><br>)',webcon).group(1)
+            con_n = re.sub('<br>','\n',con)
+            return con_n
+        except AttributeError:
+            return '广告内容，已经忽略'
 
     #写入文件
     def writeDate(self):
         con_t = self.getTitle()
         con_n = self.getCon()
-        with open('/tmp/yydlr.txt','w+') as f:
-            f.write(con_t)
-            f.write(con_n)
+        try:
+            with open('/tmp/yydlr.txt','a') as f:
+                f.write('\n')
+                f.write('\n')
+                f.write(con_t)
+                f.write(con_n)
+        except IOError:
+            print '写入异常' 
 
-#NR = NEIRONG('http://tieba.baidu.com/p/',3453463389)
-#NR.writeDate()
-ZJ = ZHANGJIE('http://tieba.baidu.com/f/good?kw=%E9%98%B4%E9%98%B3%E4%BB%A3%E7%90%86%E4%BA%BA&ie=utf-8&cid=0&pn=',3050)
-ZJ.getNum()
+for i in range(3050,1650,-50):
+    ZJ = ZHANGJIE('http://tieba.baidu.com/f/good?kw=%E9%98%B4%E9%98%B3%E4%BB%A3%E7%90%86%E4%BA%BA&ie=utf-8&cid=0&pn=',i)
+    ZJ.getNum()
+    for i in Num_r:
+        NR = NEIRONG('http://tieba.baidu.com/p/',i)
+        NR.writeDate()
+print '小说已经准备好啦'
